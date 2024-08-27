468d9e44495f9e51ccebb30ddc5b8b803e248ab2
hyunkoome <hyunkookim.me@gmail.com>
2024-08-27 18:08:27 +0900

    clean code 6

--- src/example_06_sllm_trainning.py
+++ src/example_06_sllm_trainning.py
@@ -21,6 +21,8 @@ from peft import LoraConfig, PeftModel
 from tqdm import tqdm
 import gc
 from huggingface_hub import login
+
+
 # from pathlib import Path
 
 
@@ -46,7 +48,7 @@ def make_prompt(ddl, question, query=''):
     return prompt
 
 
-def make_requests_for_gpt_evaluation(df, filename, dir='requests'):
+def make_requests_for_gpt_evaluation(df, filename, dir='./results_data/example6/requests'):
     """
     ## 예제 6.4. 평가를 위한 요청 jsonl 작성 함수
     :param df:
@@ -56,7 +58,7 @@ def make_requests_for_gpt_evaluation(df, filename, dir='requests'):
     """
 
     if not Path(dir).exists():
-        Path(dir).mkdir(parents=True)
+        Path(dir).mkdir(parents=True, exist_ok=True)
     prompts = []
     for idx, row in df.iterrows():
         prompts.append(
@@ -132,6 +134,7 @@ if __name__ == '__main__':
     Path(f"{save_root_dir}/fine_tune_train_data").mkdir(parents=True, exist_ok=True)
     save_root_dir = str(Path(save_root_dir).resolve())
     print('save_root_dir: ', save_root_dir)
+    # exit()
 
     torch.cuda.empty_cache()
 
@@ -162,77 +165,76 @@ if __name__ == '__main__':
                        return_full_text=False, max_length=512, truncation=True)
     print(gen_sqls)
 
-    print('## 예제 6.8. 기초 모델 성능 측정')
+    # print('## 예제 6.8. 기초 모델 성능 측정')
+    #
+    # print('# 데이터셋 불러오기')
+    # df = load_dataset("shangrilar/ko_text2sql", "origin")['test']
+    # df = df.to_pandas()
+    # for idx, row in tqdm(df.iterrows()):
+    #     prompt = make_prompt(row['context'], row['question'])
+    #     df.loc[idx, 'prompt'] = prompt
+    #
+    # print(df['prompt'].tolist()[0])
+    #
+    # print('# sql 생성')
+    # # gen_sqls = hf_pipe(df['prompt'].tolist(), do_sample=False,
+    # #                    return_full_text=False, max_length=512, truncation=True)
+    # # gen_sqls = [x[0]['generated_text'] for x in gen_sqls]
+    # # df['gen_sql'] = gen_sqls
+    # total_elements = len(df)
+    # for idx, row in df.iterrows():
+    #     gen_sqls = hf_pipe(row['prompt'], do_sample=False,
+    #                        return_full_text=False, max_length=512, truncation=True)
+    #     gen_sqls = gen_sqls[0]['generated_text']
+    #     df.loc[idx, 'gen_sql'] = gen_sqls
+    #     print(f'... done: {idx}/{total_elements} ')
+    #
+    # df.to_csv(f"{save_root_dir}/ko_text2sql/df_test.csv", index=False)
+    df = pd.read_csv(f"{save_root_dir}/ko_text2sql/df_test.csv")
 
-    print('# 데이터셋 불러오기')
-    df = load_dataset("shangrilar/ko_text2sql", "origin")['test']
-    df = df.to_pandas()
-    for idx, row in tqdm(df.iterrows()):
-        prompt = make_prompt(row['context'], row['question'])
-        df.loc[idx, 'prompt'] = prompt
-
-    print(df['prompt'].tolist()[0])
-
-    print('# sql 생성')
-    # gen_sqls = hf_pipe(df['prompt'].tolist(), do_sample=False,
-    #                    return_full_text=False, max_length=512, truncation=True)
-    # gen_sqls = [x[0]['generated_text'] for x in gen_sqls]
-    # df['gen_sql'] = gen_sqls
-    total_elements = len(df)
-    for idx, row in df.iterrows():
-        gen_sqls = hf_pipe(row['prompt'], do_sample=False,
-                           return_full_text=False, max_length=512, truncation=True)
-        gen_sqls = gen_sqls[0]['generated_text']
-        df.loc[idx, 'gen_sql'] = gen_sqls
-        print(f'... done: {idx}/{total_elements} ')
-
-    df.to_csv(f"{save_root_dir}/ko_text2sql/df_test.csv", index=False)
-    # df = pd.read_csv(f'{ko_text2sql_save_path}/df_test.csv')
-
-    print('# 평가를 위한 requests.jsonl 생성')
-    eval_filepath = "text2sql_evaluation.jsonl"
-    make_requests_for_gpt_evaluation(df, f"{save_root_dir}/requests/{eval_filepath}")
-
-    print('# 기초 모델 성능 측정')
-    request_url = "https://api.openai.com/v1/chat/completions"
-    api_key = os.environ["OPENAI_API_KEY"]
-    max_requests_per_minute = 200  # 300
-    max_tokens_per_minute = 100000
-    token_encoding_name = "cl100k_base"
-    max_attempts = 5
-    logging_level = 20
-
-    asyncio.run(
-        process_api_requests_from_file(
-            requests_filepath=f"{save_root_dir}/requests/{eval_filepath}",
-            save_filepath=f"{save_root_dir}/openai/{eval_filepath}",
-            request_url=request_url,
-            api_key=api_key,
-            max_requests_per_minute=float(max_requests_per_minute),
-            max_tokens_per_minute=float(max_tokens_per_minute),
-            token_encoding_name=token_encoding_name,
-            max_attempts=int(max_attempts),
-            logging_level=int(logging_level),
-        )
-    )
-
-    print('# 6.3.2 미세 조정 수행')
-    print('## 6.9 학습 데이터 불러오기')
-    df_sql = load_dataset("shangrilar/ko_text2sql", "origin")["train"]
-    df_sql = df_sql.to_pandas()
-    df_sql = df_sql.dropna().sample(frac=1, random_state=42)
-    df_sql = df_sql.query("db_id != 1")
-
-    for idx, row in tqdm(df_sql.iterrows()):
-        df_sql.loc[idx, 'text'] = make_prompt(row['context'], row['question'], row['answer'])
-    df_sql.to_csv(f"{save_root_dir}/ko_text2sql/df_sql_train.csv", index=False)
-
-    df_sql.to_csv(f"{save_root_dir}/fine_tune_train_data/train.csv", index=False)
+    # print('# 평가를 위한 requests.jsonl 생성')
+    # eval_filepath = "text2sql_evaluation.jsonl"
+    # make_requests_for_gpt_evaluation(df, f"{save_root_dir}/requests/{eval_filepath}")
+    #
+    # print('# 기초 모델 성능 측정')
+    # request_url = "https://api.openai.com/v1/chat/completions"
+    # api_key = os.environ["OPENAI_API_KEY"]
+    # max_requests_per_minute = 200  # 300
+    # max_tokens_per_minute = 100000
+    # token_encoding_name = "cl100k_base"
+    # max_attempts = 5
+    # logging_level = 20
+    #
+    # asyncio.run(
+    #     process_api_requests_from_file(
+    #         requests_filepath=f"{save_root_dir}/requests/{eval_filepath}",
+    #         save_filepath=f"{save_root_dir}/openai/{eval_filepath}",
+    #         request_url=request_url,
+    #         api_key=api_key,
+    #         max_requests_per_minute=float(max_requests_per_minute),
+    #         max_tokens_per_minute=float(max_tokens_per_minute),
+    #         token_encoding_name=token_encoding_name,
+    #         max_attempts=int(max_attempts),
+    #         logging_level=int(logging_level),
+    #     )
+    # )
+    #
+    # print('# 6.3.2 미세 조정 수행')
+    # print('## 6.9 학습 데이터 불러오기')
+    # df_sql = load_dataset("shangrilar/ko_text2sql", "origin")["train"]
+    # df_sql = df_sql.to_pandas()
+    # df_sql = df_sql.dropna().sample(frac=1, random_state=42)
+    # df_sql = df_sql.query("db_id != 1")
+    #
+    # for idx, row in tqdm(df_sql.iterrows()):
+    #     df_sql.loc[idx, 'text'] = make_prompt(row['context'], row['question'], row['answer'])
+    # df_sql.to_csv(f"{save_root_dir}/ko_text2sql/df_sql_train.csv", index=False)
+    #
+    # df_sql.to_csv(f"{save_root_dir}/fine_tune_train_data/train.csv", index=False)
 
     print('## 미세조정')
-    # base_model_name = 'beomi/Yi-Ko-6B'
-    base_model = 'beomi/Yi-Ko-6B'
-    finetuned_model = 'yi-ko-6b-text2sql'
+    base_model_name = 'beomi/Yi-Ko-6B'
+    finetuned_model_name = 'yi-ko-6b-text2sql'
 
     """
     1. AutoTrain LLM Fine-Tuning
@@ -249,11 +251,12 @@ if __name__ == '__main__':
 
     # 실행할 명령어를 문자열로 작성
     # --batch-size 8 -> 32
-    command = f"""
+    print('# autotrain llm 명령어를 실행')
+    autotrain_llm_command = f"""
             autotrain llm \
             --train \
-            --model {base_model} \
-            --project-name {finetuned_model}-full-fine-tuning \
+            --model {base_model_name} \
+            --project-name {finetuned_model_name}-full-fine-tuning \
             --data-path {save_root_dir}/fine_tune_train_data \
             --text-column text \
             --lr 2e-4 \
@@ -271,17 +274,26 @@ if __name__ == '__main__':
             --quantization int4 \
             --trainer sft \
             --username {os.getenv('HF_USERNAME')} \
-            --token {os.getenv('HF_TOKEN')}
+            --token {os.getenv('HF_TOKEN')} \
+            --push_to_hub
             """
 
-    print('# 명령어를 실행')
-    result = subprocess.run(command, shell=True)
-
+    result = subprocess.run(autotrain_llm_command, shell=True)
     if result.returncode != 0:
-        print("명령어 실행 중 오류가 발생했습니다. 함수 종료.")
+        print("autotrain llm 명령어 실행 중 오류가 발생했습니다. 함수 종료.")
         exit()
 
-    print("명령어가 성공적으로 실행되었습니다. 다음 작업을 수행합니다.")
+    print('# mv 명령어를 실행')
+    Path(f"{save_root_dir}/new_models").mkdir(parents=True, exist_ok=True)
+    mv_command = f"mv ./{finetuned_model_name}-full-fine-tuning {save_root_dir}/new_models"
+
+    result = subprocess.run(mv_command, shell=True)
+    if result.returncode != 0:
+        print("mv 명령어 실행 중 오류가 발생했습니다. 함수 종료.")
+        exit()
+
+    print("명령어가 성공적으로 실행되었습니다.")
+    print("GPU VRAM 메모리 해제합니다.")
     torch.cuda.empty_cache()
 
     print('## 예제 6.11. LoRA 어댑터 결합 및 허깅페이스 허브 업로드')
@@ -290,42 +302,54 @@ if __name__ == '__main__':
     login(token=os.getenv('HF_TOKEN'))
 
     # model_name = base_model_name
-    model_name = base_model
+    # model_name = base_model
     device_map = {"": 0}
 
     print('# LoRA와 기초 모델 파라미터 합치기')
     """
     2. LoRA + PEFT로 Fine-Tuning 및 Merge
-    - 모델 구조: 
-        PEFT의 LoRA 방법은 전체 모델이 아닌 일부 가중치만을 업데이트하는 방법입니다. 
+    - 모델 구조:
+        PEFT의 LoRA 방법은 전체 모델이 아닌 일부 가중치만을 업데이트하는 방법입니다.
         일반적으로, 대규모 모델에서 미세 조정해야 하는 파라미터의 수를 줄여 학습 비용을 절감하면서도 성능 저하를 최소화하는 방식입니다.
-    - 파라미터 업데이트: 
-        LoRA는 모델의 일부 파라미터를 저차원(low-rank) 방식으로 업데이트합니다. 
+    - 파라미터 업데이트:
+        LoRA는 모델의 일부 파라미터를 저차원(low-rank) 방식으로 업데이트합니다.
         원본 모델은 유지한 채로 추가적인 저차원 가중치를 학습하여 더 작은 모델로 학습하는 방식입니다.
-    - Merge와 Unload: 
-        model.merge_and_unload() 메서드는 LoRA를 통해 학습된 저차원 가중치를 원본 모델에 병합(merge)한 뒤, 
-            불필요한 추가 메타데이터와 가중치를 메모리에서 해제(unload)하는 과정입니다. 
-        이를 통해 최종적으로 하나의 통합된 모델이 생성됩니다. 
+    - Merge와 Unload:
+        model.merge_and_unload() 메서드는 LoRA를 통해 학습된 저차원 가중치를 원본 모델에 병합(merge)한 뒤,
+            불필요한 추가 메타데이터와 가중치를 메모리에서 해제(unload)하는 과정입니다.
+        이를 통해 최종적으로 하나의 통합된 모델이 생성됩니다.
         하지만 이 경우에도 모델의 대부분의 가중치는 원본 모델의 것이고, 미세 조정된 저차원 가중치만 합쳐진 상태입니다.
     """
     base_model = AutoModelForCausalLM.from_pretrained(
-        model_name,
+        base_model_name,
         low_cpu_mem_usage=True,
         return_dict=True,
         torch_dtype=torch.float16,
         device_map=device_map,
     )
-    model = PeftModel.from_pretrained(base_model, model_id=f"{finetuned_model}-finetune-lora-peft")
-    model = model.merge_and_unload()
+
+    # merged_model = PeftModel.from_pretrained(base_model, model_id=f"{finetuned_model_name}-full-fine-tuning")
+    merged_model = PeftModel.from_pretrained(base_model,
+                                             model_id=f"{save_root_dir}/new_models/{finetuned_model_name}-full-fine-tuning")
+    merged_model = merged_model.merge_and_unload()
+    merged_model_id = f"hyunkookim/{finetuned_model_name}-finetune-lora-peft"
+
+    output_dir = f"{save_root_dir}/new_models/{finetuned_model_name}-finetune-lora-peft"
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     print('# 토크나이저 설정')
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(base_model_name, trust_remote_code=True)
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "right"
 
     print('# 허깅페이스 허브에 모델 및 토크나이저 저장')
-    model.push_to_hub(repo_id=f"{finetuned_model}-finetune-lora-peft", use_temp_dir=False)
-    tokenizer.push_to_hub(repo_id=f"{finetuned_model}-finetune-lora-peft", use_temp_dir=False)
+    merged_model.save_pretrained(output_dir)
+    tokenizer.save_pretrained(output_dir)
+
+    # merged_model.push_to_hub(repo_id=f"{finetuned_model_name}-finetune-lora-peft", use_temp_dir=False)
+    # tokenizer.push_to_hub(repo_id=f"{finetuned_model_name}-finetune-lora-peft", use_temp_dir=False)
+    merged_model.push_to_hub(merged_model_id, working_dir=output_dir)
+    tokenizer.push_to_hub(merged_model_id, working_dir=output_dir)
 
     """
     * 차이점 요약
@@ -344,7 +368,7 @@ if __name__ == '__main__':
         - 효율성을 중시할 때는 LoRA + PEFT 방식이 적합합니다.
     """
     print('## 예제 6.12. 미세 조정한 모델(AutoTrain LLM)로 예시 데이터에 대한 SQL 생성')
-    model_id = f"hyunkookim/{finetuned_model}-full-fine-tuning"
+    model_id = f"hyunkookim/{finetuned_model_name}-full-fine-tuning"
     hf_pipe = make_inference_pipeline(model_id=model_id)
 
     gen_sqls = hf_pipe(example, do_sample=False,
@@ -352,7 +376,7 @@ if __name__ == '__main__':
     print(gen_sqls)
     # SELECT COUNT(*) FROM players WHERE username LIKE '%admin%';
 
-    print('## 예제 6.13. 미세 조정한 모델 성능 측정')
+    print('## 예제 6.13. 미세 조정한 모델(AutoTrain LLM) 성능 측정')
     # sql 생성 수행
     # gen_sqls = hf_pipe(df['prompt'].tolist(), do_sample=False,
     #                    return_full_text=False, max_length=1024, truncation=True)
@@ -365,10 +389,10 @@ if __name__ == '__main__':
         gen_sqls = gen_sqls[0]['generated_text']
         df.loc[idx, 'gen_sql'] = gen_sqls
         print(f'... done: {idx}/{total_elements} ')
-    df.to_csv(f"{save_root_dir}/ko_text2sql/df_text2sql_evaluation_{finetuned_model}-full-fine-tuning.csv", index=False)
+    df.to_csv(f"{save_root_dir}/ko_text2sql/df_text2sql_evaluation_{finetuned_model_name}-full-fine-tuning.csv", index=False)
 
     print('# 평가를 위한 requests.jsonl 생성')
-    ft_eval_filepath = f"text2sql_evaluation_{finetuned_model}-full-fine-tuning.jsonl"
+    ft_eval_filepath = f"text2sql_evaluation_{finetuned_model_name}-full-fine-tuning.jsonl"
     make_requests_for_gpt_evaluation(df,
                                      f"{save_root_dir}/requests/{ft_eval_filepath}")
 
@@ -415,7 +439,7 @@ if __name__ == '__main__':
     print(num_correct_answers)
 
     print('## 예제 6.14. 미세 조정한 모델(LoRA + PEFT)로 예시 데이터에 대한 SQL 생성')
-    model_id = f"hyunkookim/{finetuned_model}-finetune-lora-peft"
+    model_id = f"hyunkookim/{finetuned_model_name}-finetune-lora-peft"
     hf_pipe = make_inference_pipeline(model_id=model_id)
 
     gen_sqls = hf_pipe(example, do_sample=False,
@@ -423,7 +447,7 @@ if __name__ == '__main__':
     print(gen_sqls)
     # SELECT COUNT(*) FROM players WHERE username LIKE '%admin%';
 
-    print('## 예제 6.13. 미세 조정한 모델 성능 측정')
+    print('## 예제 6.13. 미세 조정한 모델(LoRA + PEFT) 성능 측정')
     # sql 생성 수행
     # gen_sqls = hf_pipe(df['prompt'].tolist(), do_sample=False,
     #                    return_full_text=False, max_length=1024, truncation=True)
@@ -436,11 +460,11 @@ if __name__ == '__main__':
         gen_sqls = gen_sqls[0]['generated_text']
         df.loc[idx, 'gen_sql'] = gen_sqls
         print(f'... done: {idx}/{total_elements} ')
-    df.to_csv(f"{save_root_dir}/ko_text2sql/df_text2sql_evaluation_{finetuned_model}-fullfine-lora-peft.csv",
+    df.to_csv(f"{save_root_dir}/ko_text2sql/df_text2sql_evaluation_{finetuned_model_name}-fullfine-lora-peft.csv",
               index=False)
 
     print('# 평가를 위한 requests.jsonl 생성')
-    ft_eval_filepath = f"text2sql_evaluation_{finetuned_model}-fullfine-lora-peft.jsonl"
+    ft_eval_filepath = f"text2sql_evaluation_{finetuned_model_name}-fullfine-lora-peft.jsonl"
     make_requests_for_gpt_evaluation(df, f"{save_root_dir}/requests/{ft_eval_filepath}")
 
     print('# GPT-4 평가 수행')
